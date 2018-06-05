17cdbe306eb9478e3acd7e7d15d4f5b4822a77ef
Xavier Tilley <hippietilley@gmail.com>
2018-06-05 16:48:25 -0500

    Documented ConfigCondition

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/configuration/ConfigCondition.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/configuration/ConfigCondition.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.md2k.ema_scheduler.configuration;
 
 import org.md2k.datakitapi.source.datasource.DataSource;
@@ -5,7 +32,7 @@ import org.md2k.datakitapi.source.datasource.DataSource;
 import java.util.ArrayList;
 
 /**
- * Created by monowar on 3/10/16.
+ * Defines a <code>ConfigCondition</code> object.
  */
 public class ConfigCondition {
     private String id;
@@ -15,39 +42,74 @@ public class ConfigCondition {
     private DataSource data_source;
     private Source source;
 
+    /**
+     * Returns the id.
+     * @return The id.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns the name.
+     * @return The name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns an arraylist of values.
+     * @return An arraylist of values.
+     */
     public ArrayList<String> getValues() {
         return values;
     }
 
 
+    /**
+     * Returns the source.
+     * @return The source.
+     */
     public Source getSource() {
         return source;
     }
 
+    /**
+     * Returns the type.
+     * @return The type.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Returns the data source.
+     * @return The data source.
+     */
     public DataSource getData_source() {
         return data_source;
     }
 
+    /**
+     * Nested class defining a <code>Source</code> object.
+     */
     public class Source{
         String id;
         String type;
 
+        /**
+         * Returns the id.
+         * @return The id.
+         */
         public String getId() {
             return id;
         }
 
+        /**
+         * Returns the type.
+         * @return The type.
+         */
         public String getType() {
             return type;
         }
