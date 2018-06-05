820f3654489a2a3d5f4da97a4ae2e2f9d37d1cf8
Xavier Tilley <hippietilley@gmail.com>
2018-06-05 15:18:35 -0500

    Documented ActivityIncentive

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/incentive/ActivityIncentive.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/incentive/ActivityIncentive.java
@@ -1,18 +1,6 @@
-package org.md2k.ema_scheduler.incentive;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import org.md2k.ema_scheduler.R;
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,33 +25,58 @@ import org.md2k.ema_scheduler.R;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+package org.md2k.ema_scheduler.incentive;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.md2k.ema_scheduler.R;
+
+/**
+ * Activity for displaying the incentive.
+ */
 public class ActivityIncentive extends AppCompatActivity {
     private static final String TAG = ActivityIncentive.class.getSimpleName();
     private Handler handler;
-    private Runnable runnableClose=new Runnable() {
+    private Runnable runnableClose = new Runnable() {
+        /**
+         * Finishes this activity.
+         */
         @Override
         public void run() {
             finish();
         }
     };
 
+    /**
+     * Sets the view and posts a delayed <code>runnable</code> to the <code>handler</code>.
+     * @param savedInstanceState Previous state of this activity, if it existed.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incentive);
-        handler=new Handler();
+        handler = new Handler();
         setCancelButton();
-        String messages[]=getIntent().getStringArrayExtra("messages");
-        double total_incentive=getIntent().getDoubleExtra("total_incentive",0);
-        if(messages[0]!=null && messages[0].length()!=0)
+        String messages[] = getIntent().getStringArrayExtra("messages");
+        double total_incentive = getIntent().getDoubleExtra("total_incentive",0);
+        if(messages[0] != null && messages[0].length() != 0)
             ((TextView)findViewById(R.id.textView_message_1)).setText(messages[0]);
-        else ((TextView)findViewById(R.id.textView_message_1)).setText("");
-        if(messages[1]!=null && messages[1].length()!=0)
+        else
+            ((TextView)findViewById(R.id.textView_message_1)).setText("");
+        if(messages[1] != null && messages[1].length() != 0)
             ((TextView)findViewById(R.id.textView_message_2)).setText(messages[1]);
         else
             ((TextView)findViewById(R.id.textView_message_2)).setText("");
-        if(messages[2]!=null && messages[2].length()!=0)
-            ((TextView)findViewById(R.id.textView_message_3)).setText(messages[2]+" "+String.format("%.2f",total_incentive));
+        if(messages[2] != null && messages[2].length() != 0)
+            ((TextView)findViewById(R.id.textView_message_3)).setText(messages[2] + " " +
+                    String.format("%.2f",total_incentive));
         else
             ((TextView)findViewById(R.id.textView_message_3)).setText("");
         handler.postDelayed(runnableClose, 60000);
@@ -71,11 +84,22 @@ public class ActivityIncentive extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    /**
+     * Creates the options menu.
+     * @param menu Menu to inflate.
+     * @return Always returns true.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
+
+    /**
+     * Finishes the activity if the home button is pressed on the device.
+     * @param item Menu item that was selected.
+     * @return Whether home or back was pressed.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -85,15 +109,22 @@ public class ActivityIncentive extends AppCompatActivity {
         }
         return true;
     }
+
+    /**
+     * Creates a button to close the activity and sets an <code>onClickListener</code>.
+     */
     private void setCancelButton() {
         final Button button = (Button) findViewById(R.id.button_1);
         button.setText("Close");
         button.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Removes the callbacks for <code>runnableClose</code> and calls <code>finish()</code>.
+             * @param v Button clicked.
+             */
             public void onClick(View v) {
                 handler.removeCallbacks(runnableClose);
                 finish();
             }
         });
     }
-
 }
