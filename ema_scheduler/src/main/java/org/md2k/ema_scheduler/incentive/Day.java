594704db3b226a9efaa684f548cd4274059d99ab
Xavier Tilley <hippietilley@gmail.com>
2018-06-05 14:50:57 -0500

    Documented Day

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/incentive/Day.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/incentive/Day.java
@@ -1,21 +1,6 @@
-package org.md2k.ema_scheduler.incentive;
-
-import android.content.Context;
-
-import org.md2k.datakitapi.DataKitAPI;
-import org.md2k.datakitapi.datatype.DataType;
-import org.md2k.datakitapi.datatype.DataTypeLong;
-import org.md2k.datakitapi.exception.DataKitException;
-import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
-import org.md2k.datakitapi.source.datasource.DataSourceClient;
-import org.md2k.datakitapi.source.datasource.DataSourceType;
-import org.md2k.utilities.Report.Log;
-
-import java.util.ArrayList;
-
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,21 +24,50 @@ import java.util.ArrayList;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package org.md2k.ema_scheduler.incentive;
+
+import android.content.Context;
+
+import org.md2k.datakitapi.DataKitAPI;
+import org.md2k.datakitapi.datatype.DataType;
+import org.md2k.datakitapi.datatype.DataTypeLong;
+import org.md2k.datakitapi.exception.DataKitException;
+import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
+import org.md2k.datakitapi.source.datasource.DataSourceClient;
+import org.md2k.datakitapi.source.datasource.DataSourceType;
+import org.md2k.utilities.Report.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Provides methods for getting start and end times for the day.
+ */
 class Day {
     private static final String TAG = Day.class.getSimpleName();
     private Context context;
+
+    /**
+     * Constructor
+     * @param context Android context
+     */
     private Day(Context context){
-        this.context=context;
+        this.context = context;
     }
 
+    /**
+     * Returns the <code>dayStartTime</code> from <code>DataKit</code>.
+     * @return The <code>dayStartTime</code>.
+     * @throws DataKitException
+     */
     public long readDayStartFromDataKit() throws DataKitException {
         long dayStartTime;
-        DataKitAPI dataKitAPI=DataKitAPI.getInstance(context);
+        DataKitAPI dataKitAPI = DataKitAPI.getInstance(context);
         Log.d(TAG, "readDayStartFromDataKit()...");
         ArrayList<DataSourceClient> dataSourceClients;
         dayStartTime = -1;
         dataSourceClients = dataKitAPI.find(new DataSourceBuilder().setType(DataSourceType.DAY_START));
-        Log.d(TAG,"readDayStartFromDataKit()...find..dataSourceClient.size()="+dataSourceClients.size());
+        Log.d(TAG,"readDayStartFromDataKit()...find..dataSourceClient.size()=" + dataSourceClients.size());
         if (dataSourceClients.size() > 0) {
             ArrayList<DataType> dataTypes = dataKitAPI.query(dataSourceClients.get(0), 1);
             if (dataTypes.size() != 0) {
@@ -64,6 +78,11 @@ class Day {
         return dayStartTime;
     }
 
+    /**
+     * Returns the <code>dayEndTime</code> from <code>DataKit</code>.
+     * @return The <code>dayEndTime</code>.
+     * @throws DataKitException
+     */
     public long readDayEndFromDataKit() throws DataKitException {
         long dayEndTime=-1;
         DataKitAPI dataKitAPI=DataKitAPI.getInstance(context);
