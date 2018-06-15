a7bfae2c2eb1c670e076557a6a6ce5cddfab5387
Xavier Tilley <hippietilley@gmail.com>
2018-06-15 13:42:53 -0500

    Documented BonusIncentive

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/bonus_incentive/BonusIncentive.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/bonus_incentive/BonusIncentive.java
@@ -1,26 +1,6 @@
-package org.md2k.ema_scheduler.condition.bonus_incentive;
-
-import android.content.Context;
-
-import com.google.gson.Gson;
-
-import org.md2k.datakitapi.DataKitAPI;
-import org.md2k.datakitapi.datatype.DataType;
-import org.md2k.datakitapi.datatype.DataTypeDouble;
-import org.md2k.datakitapi.datatype.DataTypeJSONObject;
-import org.md2k.datakitapi.exception.DataKitException;
-import org.md2k.datakitapi.source.datasource.DataSource;
-import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
-import org.md2k.datakitapi.source.datasource.DataSourceClient;
-import org.md2k.datakitapi.time.DateTime;
-import org.md2k.ema_scheduler.condition.Condition;
-import org.md2k.ema_scheduler.configuration.ConfigCondition;
-import org.md2k.ema_scheduler.incentive.Incentive;
-import java.util.ArrayList;
-
-/**
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+/*
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,18 +24,50 @@ import java.util.ArrayList;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package org.md2k.ema_scheduler.condition.bonus_incentive;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import org.md2k.datakitapi.DataKitAPI;
+import org.md2k.datakitapi.datatype.DataType;
+import org.md2k.datakitapi.datatype.DataTypeDouble;
+import org.md2k.datakitapi.datatype.DataTypeJSONObject;
+import org.md2k.datakitapi.exception.DataKitException;
+import org.md2k.datakitapi.source.datasource.DataSource;
+import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
+import org.md2k.datakitapi.source.datasource.DataSourceClient;
+import org.md2k.datakitapi.time.DateTime;
+import org.md2k.ema_scheduler.condition.Condition;
+import org.md2k.ema_scheduler.configuration.ConfigCondition;
+import org.md2k.ema_scheduler.incentive.Incentive;
+import java.util.ArrayList;
+
+/**
+ *
+ */
 public class BonusIncentive extends Condition{
+    /**
+     * Constructor
+     * @param context Android context
+     */
     public BonusIncentive(Context context){
         super(context);
     }
+
+    /**
+     * Returns whether the condition is valid.
+     * @param configCondition Configuration of the condition.
+     * @return Whether the condition is valid.
+     * @throws DataKitException
+     */
     public boolean isValid(ConfigCondition configCondition) throws DataKitException {
-        //if(true) return true;
-        long curTime= DateTime.getDateTime();
-//        long prevTime= curTime - Integer.parseInt(configCondition.getValues().get(0));
-//        int sampleNo = (Integer.parseInt(configCondition.getValues().get(0))) / 60000;
+        long curTime = DateTime.getDateTime();
         int value = Integer.parseInt(configCondition.getValues().get(0));
-        double incentiveValue=Double.parseDouble(configCondition.getValues().get(1));
-        DataKitAPI dataKitAPI=DataKitAPI.getInstance(context);
+        double incentiveValue = Double.parseDouble(configCondition.getValues().get(1));
+        DataKitAPI dataKitAPI = DataKitAPI.getInstance(context);
         DataSource dataSource = configCondition.getData_source();
         DataSourceBuilder dataSourceBuilder = new DataSourceBuilder(dataSource);
         ArrayList<DataSourceClient> dataSourceClientArrayList = dataKitAPI.find(dataSourceBuilder);
@@ -65,22 +77,23 @@ public class BonusIncentive extends Condition{
                 log(configCondition, "true: data point not found");
                 return true;
             }
-            int count=0;
-            Gson gson=new Gson();
+            int count = 0;
+            Gson gson = new Gson();
             for (int i = 0; i < dataTypes.size(); i++) {
-                DataTypeJSONObject dataTypeJSONObject= (DataTypeJSONObject) dataTypes.get(i);
-                Incentive incentive=gson.fromJson(dataTypeJSONObject.getSample(),Incentive.class);
-                if(incentive.getIncentive()==incentiveValue)
+                DataTypeJSONObject dataTypeJSONObject = (DataTypeJSONObject) dataTypes.get(i);
+                Incentive incentive = gson.fromJson(dataTypeJSONObject.getSample(), Incentive.class);
+                if(incentive.getIncentive() == incentiveValue)
                     count++;
             }
             if(count<value) {
-                log(configCondition, "true: Bonus incentive provided "+String.valueOf(count)+" < "+String.valueOf(value));
+                log(configCondition, "true: Bonus incentive provided " + String.valueOf(count) +
+                        " < " + String.valueOf(value));
                 return true;
             } else {
-                log(configCondition, "false: Bonus incentive provided "+String.valueOf(count)+" >= "+String.valueOf(value));
+                log(configCondition, "false: Bonus incentive provided " + String.valueOf(count) +
+                        " >= " + String.valueOf(value));
                 return false;
             }
-
         } else {
             log(configCondition, "true: datasource not found");
             return true;
