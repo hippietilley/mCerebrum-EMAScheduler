c2473ab178230ec9ba2d5cb09f8e1defcec9abf6
Xavier Tilley <hippietilley@gmail.com>
2018-06-15 15:36:51 -0500

    Documented Condition

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/Condition.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/Condition.java
@@ -1,16 +1,6 @@
-package org.md2k.ema_scheduler.condition;
-
-import android.content.Context;
-
-import org.md2k.datakitapi.exception.DataKitException;
-import org.md2k.datakitapi.time.DateTime;
-import org.md2k.ema_scheduler.configuration.ConfigCondition;
-import org.md2k.ema_scheduler.logger.LogInfo;
-import org.md2k.ema_scheduler.logger.LoggerManager;
-
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,17 +24,48 @@ import org.md2k.ema_scheduler.logger.LoggerManager;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package org.md2k.ema_scheduler.condition;
+
+import android.content.Context;
+
+import org.md2k.datakitapi.exception.DataKitException;
+import org.md2k.datakitapi.time.DateTime;
+import org.md2k.ema_scheduler.configuration.ConfigCondition;
+import org.md2k.ema_scheduler.logger.LogInfo;
+import org.md2k.ema_scheduler.logger.LoggerManager;
+
+/**
+ * Defines a condition.
+ */
 public abstract class Condition {
     private static final String TAG = Condition.class.getSimpleName();
     protected Context context;
+
+    /**
+     * Constructor
+     * @param context Android context
+     */
     protected Condition(Context context){
-        this.context=context;
+        this.context = context;
     }
 
+    /**
+     * Returns whether the condition is valid.
+     * @param configCondition Configuration of the condition.
+     * @return Whether the condition is valid.
+     * @throws DataKitException
+     */
     public abstract boolean isValid(ConfigCondition configCondition) throws DataKitException;
 
+    /**
+     * Logs the given message for the given condition configuration.
+     * @param configCondition  Configuration of the condition.
+     * @param message Message to log.
+     * @throws DataKitException
+     */
     protected void log(ConfigCondition configCondition, String message) throws DataKitException {
-        LogInfo logInfo=new LogInfo();
+        LogInfo logInfo = new LogInfo();
         logInfo.setOperation(LogInfo.OP_CONDITION);
         logInfo.setId(configCondition.getId());
         logInfo.setType(configCondition.getType());
