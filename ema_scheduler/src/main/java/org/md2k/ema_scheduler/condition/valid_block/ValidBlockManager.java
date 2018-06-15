e2778572a426aa56397d03ae3716530dc28ba004
Xavier Tilley <hippietilley@gmail.com>
2018-06-15 15:31:03 -0500

    Documented ValidBlockManager

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/valid_block/ValidBlockManager.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/valid_block/ValidBlockManager.java
@@ -1,28 +1,6 @@
-package org.md2k.ema_scheduler.condition.valid_block;
-
-import android.content.Context;
-
-import org.md2k.datakitapi.DataKitAPI;
-import org.md2k.datakitapi.datatype.DataType;
-import org.md2k.datakitapi.datatype.DataTypeLong;
-import org.md2k.datakitapi.exception.DataKitException;
-import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
-import org.md2k.datakitapi.source.datasource.DataSourceClient;
-import org.md2k.datakitapi.source.datasource.DataSourceType;
-import org.md2k.datakitapi.time.DateTime;
-import org.md2k.ema_scheduler.condition.Condition;
-import org.md2k.ema_scheduler.configuration.Block;
-import org.md2k.ema_scheduler.configuration.ConfigCondition;
-import org.md2k.ema_scheduler.configuration.Configuration;
-import org.md2k.ema_scheduler.logger.LogInfo;
-import org.md2k.ema_scheduler.logger.LoggerManager;
-import org.md2k.utilities.Report.Log;
-
-import java.util.ArrayList;
-
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -46,62 +24,113 @@ import java.util.ArrayList;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package org.md2k.ema_scheduler.condition.valid_block;
+
+import android.content.Context;
+
+import org.md2k.datakitapi.DataKitAPI;
+import org.md2k.datakitapi.datatype.DataType;
+import org.md2k.datakitapi.datatype.DataTypeLong;
+import org.md2k.datakitapi.exception.DataKitException;
+import org.md2k.datakitapi.source.datasource.DataSourceBuilder;
+import org.md2k.datakitapi.source.datasource.DataSourceClient;
+import org.md2k.datakitapi.source.datasource.DataSourceType;
+import org.md2k.datakitapi.time.DateTime;
+import org.md2k.ema_scheduler.condition.Condition;
+import org.md2k.ema_scheduler.configuration.Block;
+import org.md2k.ema_scheduler.configuration.ConfigCondition;
+import org.md2k.ema_scheduler.configuration.Configuration;
+import org.md2k.ema_scheduler.logger.LogInfo;
+import org.md2k.ema_scheduler.logger.LoggerManager;
+import org.md2k.utilities.Report.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Manages the valid block condition.
+ */
 public class ValidBlockManager extends Condition {
     private static final String TAG = ValidBlockManager.class.getSimpleName();
 
+    /**
+     * Constructor
+     * @param context Android context
+     */
     public ValidBlockManager(Context context){
         super(context);
     }
+
+    /**
+     * Returns the day for the given data source type.
+     * @param dataSourceType Data source type.
+     * @return The day.
+     * @throws DataKitException
+     */
     public boolean isValid(ConfigCondition configCondition) throws DataKitException {
-//        if(Constants.DEBUG) return true;
-        long curTime=DateTime.getDateTime();
-        long dayStart=getDay(DataSourceType.DAY_START);
-        long dayEnd=getDay(DataSourceType.DAY_END);
-        if(dayStart==-1){
-            log(configCondition, "false: day is not started"+" DayStart="+dayStart+" DayEnd="+dayEnd);
+        long curTime = DateTime.getDateTime();
+        long dayStart = getDay(DataSourceType.DAY_START);
+        long dayEnd = getDay(DataSourceType.DAY_END);
+        if(dayStart== -1){
+            log(configCondition, "false: day is not started" + " DayStart=" + dayStart + " DayEnd=" + dayEnd);
             return false;
         }else if(dayStart<dayEnd){
-            log(configCondition, "false: new day is not started"+" DayStart="+dayStart+" DayEnd="+dayEnd);
+            log(configCondition, "false: new day is not started" + " DayStart=" + dayStart + " DayEnd=" + dayEnd);
             return false;
         }else {
-            Block[] blocks=Configuration.getInstance().getEma_types(configCondition.getSource().getId(), configCondition.getSource().getType()).getBlocks();
-            for(int i=0;i<blocks.length;i++){
-                long blockStartTime=blocks[i].getStart_offset()+dayStart;
-                long blockEndTime=blocks[i].getEnd_offset()+dayStart;
+            Block[] blocks = Configuration.getInstance()
+                    .getEma_types(configCondition.getSource().getId(), configCondition.getSource().getType()).getBlocks();
+            for(int i = 0; i < blocks.length; i++){
+                long blockStartTime = blocks[i].getStart_offset() + dayStart;
+                long blockEndTime = blocks[i].getEnd_offset() + dayStart;
                 Log.d(TAG, "blockStartTime=" + blockStartTime + " curTime=" + curTime + " blockEndTime=" + blockEndTime);
-                int count=0;
+                int count = 0;
                 if(blockStartTime<=curTime && curTime<blockEndTime){
-                    int limitEMA=Integer.parseInt(configCondition.getValues().get(0));
-                    ArrayList<LogInfo> logInfos=LoggerManager.getInstance(context).getLogInfos(LogInfo.OP_DELIVER,LogInfo.STATUS_DELIVER_SUCCESS, configCondition.getSource().getType(), configCondition.getSource().getId());
-                    for(int j=0;j<logInfos.size();j++){
-                        if(blockStartTime<=logInfos.get(j).getTimestamp() && logInfos.get(j).getTimestamp()<blockEndTime)
+                    int limitEMA = Integer.parseInt(configCondition.getValues().get(0));
+                    ArrayList<LogInfo> logInfos = LoggerManager.getInstance(context)
+                            .getLogInfos(LogInfo.OP_DELIVER,LogInfo.STATUS_DELIVER_SUCCESS,
+                                    configCondition.getSource().getType(), configCondition.getSource().getId());
+                    for(int j = 0; j < logInfos.size(); j++){
+                        if(blockStartTime <= logInfos.get(j).getTimestamp() &&
+                                logInfos.get(j).getTimestamp() < blockEndTime)
                             count++;
                     }
-                    if(limitEMA>count) {
-                        log(configCondition, "true: block(" + String.valueOf(i + 1)+") triggered: "+String.valueOf(count)+" expected: "+String.valueOf(limitEMA)+" DayStart="+dayStart+" DayEnd="+dayEnd);
+                    if(limitEMA > count) {
+                        log(configCondition, "true: block(" + String.valueOf(i + 1) + ") triggered: " +
+                                String.valueOf(count) + " expected: " + String.valueOf(limitEMA) +
+                                " DayStart=" + dayStart + " DayEnd=" + dayEnd);
                         return true;
                     }else{
-                        log(configCondition, "false: block(" + String.valueOf(i + 1)+") triggered: "+String.valueOf(count)+" expected: "+String.valueOf(limitEMA)+" DayStart="+dayStart+" DayEnd="+dayEnd);
+                        log(configCondition, "false: block(" + String.valueOf(i + 1) + ") triggered: " +
+                                String.valueOf(count) + " expected: " + String.valueOf(limitEMA) +
+                                " DayStart=" + dayStart + " DayEnd=" + dayEnd);
                         return false;
                     }
                 }
             }
-            log(configCondition,"false: no valid block found"+" DayStart="+dayStart+" DayEnd="+dayEnd);
+            log(configCondition, "false: no valid block found" + " DayStart=" + dayStart +
+                    " DayEnd=" + dayEnd);
             return false;
         }
     }
 
+    /**
+     * Returns the day for the given data source type.
+     * @param dataSourceType Data source type.
+     * @return The day.
+     * @throws DataKitException
+     */
     private long getDay(String dataSourceType) throws DataKitException {
-        long day=-1;
-        DataKitAPI dataKitAPI=DataKitAPI.getInstance(context);
-        DataSourceBuilder dataSourceBuilder=new DataSourceBuilder().setType(dataSourceType);
-        ArrayList<DataSourceClient> dataSourceClients=dataKitAPI.find(dataSourceBuilder);
-
-        if(dataSourceClients.size()==0){
+        long day = -1;
+        DataKitAPI dataKitAPI = DataKitAPI.getInstance(context);
+        DataSourceBuilder dataSourceBuilder = new DataSourceBuilder().setType(dataSourceType);
+        ArrayList<DataSourceClient> dataSourceClients = dataKitAPI.find(dataSourceBuilder);
+        if(dataSourceClients.size() == 0){
             return day;
         }
-        ArrayList<DataType> dataTypes=dataKitAPI.query(dataSourceClients.get(0),1);
-        if(dataTypes.size()==0) return day;
+        ArrayList<DataType> dataTypes = dataKitAPI.query(dataSourceClients.get(0), 1);
+        if(dataTypes.size() == 0)
+            return day;
         return ((DataTypeLong)dataTypes.get(0)).getSample();
     }
 }
