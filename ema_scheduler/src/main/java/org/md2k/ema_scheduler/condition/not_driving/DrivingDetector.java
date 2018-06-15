a465244451130d265fe430a719aee209741f5c24
Xavier Tilley <hippietilley@gmail.com>
2018-06-15 14:57:31 -0500

    Documented DrivingDetector

--- ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/not_driving/DrivingDetector.java
+++ ema_scheduler/src/main/java/org/md2k/ema_scheduler/condition/not_driving/DrivingDetector.java
@@ -1,8 +1,6 @@
-package org.md2k.ema_scheduler.condition.not_driving;
-
 /*
- * Copyright (c) 2016, The University of Memphis, MD2K Center
- * - Syed Monowar Hossain <monowar.hossain@gmail.com>
+ * Copyright (c) 2018, The University of Memphis, MD2K Center of Excellence
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,24 +24,46 @@ package org.md2k.ema_scheduler.condition.not_driving;
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package org.md2k.ema_scheduler.condition.not_driving;
+
+/**
+ * Determines if the device is in a moving vehicle.
+ */
 class DrivingDetector {
 
-    public static final double maxStopSignTime = 5*60*1000; // 5 minute
+    public static final double maxStopSignTime = 5 * 60 * 1000; // 5 minute
     /**
      * Bohannon, R. Comfortable and maximum walking speed of adults aged 20-79 years: Reference values and determinants. Age and Ageing 26, 1 (1997), 15–19.
      */
-    private static final double maxGaitSpeed = 2.533; // meter/second
-    private static final double maxGpsMissingTime = 30*1000; // 30 second
+    private static final double maxGaitSpeed = 2.533; // meters per second
+    private static final double maxGpsMissingTime = 30 * 1000; // 30 second
 
+    /**
+     * Returns the driving status.
+     * @param lastTimestamp Last recorded timestamp.
+     * @param currentTimestamp Current timestamp.
+     * @param speed Current speed.
+     * @return The driving status.
+     */
     DrivingStatus getDrivingStatus(long lastTimestamp, long currentTimestamp, double speed) {
-        if(lastTimestamp==-1 || currentTimestamp-lastTimestamp > maxGpsMissingTime) {
+        if(lastTimestamp == -1 || currentTimestamp - lastTimestamp > maxGpsMissingTime) {
             return DrivingStatus.UNKNOWN;
         }
-        if(speed>maxGaitSpeed) return DrivingStatus.DRIVING;
+        if(speed > maxGaitSpeed)
+            return DrivingStatus.DRIVING;
         else return DrivingStatus.NOT_DRIVING;
     }
 
-
+    /**
+     * Enumeration of possible driving statuses:
+     * <ul>
+     *     <li>Driving</li>
+     *     <li>Driving, but stopped at a stop sign</li>
+     *     <li>Not driving</li>
+     *     <li>Unknown</li>
+     * </ul>
+     */
     enum DrivingStatus {
         DRIVING,
         DRIVING_STOP_SIGN,
